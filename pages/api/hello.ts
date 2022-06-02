0c28cbdaaf2026a16e48a57f64c4f5ba53e54e53
Ishaq Ibrahim <ishaq.ibrahim@teamo.io>
2022-06-02 07:51:32 +0500

    feat: add api call to fetch jira issues

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  issues: any;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const emailId = "ishaq.ibrahim@convertdigital.com.au";
+  const baseUrl = "https://convertdigital.atlassian.net/rest/api/3/";
+  const authToken =
+    "aXNoYXEuaWJyYWhpbUBjb252ZXJ0ZGlnaXRhbC5jb20uYXU6RzJqcFZveHlYQ0ttemtxaTdxVmVGMEQ1";
+  const url = new URL(
+    `search?jql=assignee='${encodeURI(emailId)}'`,
+    baseUrl
+  ).toString();
+  console.log("URL: ", url);
+  const response = await fetch(url, {
+    headers: {
+      Authorization: "Basic " + authToken,
+    },
+  });
+  console.log("RESPONSE: ", response);
+  const issues = await response.json();
+  res.status(200).json({ issues });
 }
